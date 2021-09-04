8d356ef6aa3b23675f69ae37d3bd86043a19351c
estebancasalas <e.casalas@outlook.com>
2021-09-04 11:45:16 -0300

    Borre los comentarios y los pase a master.

--- Ejercicio 1/src/Library/Libro.cs
+++ Ejercicio 1/src/Library/Libro.cs
@@ -5,33 +5,15 @@ namespace SRP
     public class Libro
     {
 
-        public string Titulo { get ; private set; } // Faltan metodos set privados
-        public string Autor { get ; private set; } // Faltan metodos set privados
-        public string Codigo { get ; private set; } // Faltan metodos set privados
-        /*
-        public string SectorBiblioteca { get ; set; }  // Atributos que le deberían corresponder a otra clase
-        public string EstanteBiblioteca { get ; set; } // Atributos que le deberían corresponder a otra clase
-        */
-
+        public string Titulo { get ; private set; }
+        public string Autor { get ; private set; } 
+        public string Codigo { get ; private set; }
+        
         public Libro(String titulo, String autor, String codigo)
         {
             this.Titulo = titulo;
             this.Autor = autor;
             this.Codigo = codigo;
         }
-
-        /* 
-        El metodo no corresponde con la clase libro
-        
-        public void AlmacenarLibro(String sector, String estante)  
-        {
-            this.SectorBiblioteca = sector;
-            this.EstanteBiblioteca = estante;
-        } 
-        */
-
     }
 }
- /* Segun el principio SRP(Single Responsibility Principle) cada objeto debe tener una unica responsabilidad. En este
- caso, podemos asignarle a la clase libro la responsabilidad de conocer los datos del libro, crear otra clase que se
- encargue de conocer la ubicación, y otra que se encargue de almacenarlo. */
